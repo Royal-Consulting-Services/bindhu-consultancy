65283c144a579368b9c982c2e210fb758a7ae103
sai krishna <saikrishna91899@gmail.com>
2023-01-21 17:50:25 +0530

    Fix: maps added

--- client/src/components/contact/Contact.js
+++ client/src/components/contact/Contact.js
@@ -69,13 +69,15 @@ function Contact() {
               <span>Location</span>
             </h3>
             <Col xs={12} md={12} className='mt-3'>
-              <iframe
-                src='https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d2965.0824050173574!2d-93.63905729999999!3d41.998507000000004!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1sWebFilings%2C+University+Boulevard%2C+Ames%2C+IA!5e0!3m2!1sen!2sus!4v1390839289319'
+               <iframe
+                src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3348.812476649048!2d-96.95720178533168!3d32.929552383500365!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x864c2857c5a745b1%3A0x5cbed32899b9dd62!2s607%20Mission%20Cir%2C%20Irving%2C%20TX%2075063%2C%20USA!5e0!3m2!1sen!2sin!4v1674303274038!5m2!1sen!2sin'
                 width='100%'
                 height='400'
-                frameborder='0'
                 style={{ border: '0' }}
-              />
+                allowfullscreen=''
+                loading='lazy'
+                referrerpolicy='no-referrer-when-downgrade'
+              ></iframe>
             </Col>
           </ScrollAnimation>
         </Row>
