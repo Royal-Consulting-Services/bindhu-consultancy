70f099a3b88f5602d4df38d3950b1971a6450759
Manubolu saikrishna <saikrishna91899@gmail.com>
2023-05-31 12:45:00 +0530

    fix token issue

--- server/verifyToken.js
+++ server/verifyToken.js
@@ -1,24 +1,30 @@
-const jwt = require('jsonwebtoken')
+const jwt = require("jsonwebtoken");
 const { secret } = require("./models/config");
 
-const verifyToken = (req, res, next) => {
-    const token = req.headers.authorization.split(' ')[1];
-
-    if(!token){
-        res.status(403).json({
-            message: 'token required for authentication'
-        })
-    } else {
-        try {
-            const decodedToken = jwt.verify(token, secret)
-            req.decodedToken = decodedToken;
-        } catch (error) {
-            res.status(401).json({
-                message: 'Invalid token received'
-            })
-        }
-    }
-    return next();
+function generateAccessToken(username) {
+  return jwt.sign(username, secret, { expiresIn: "1800s" });
 }
+
+const verifyToken = (req, res, next) => {
+  const token = generateAccessToken({ userName: req.body.userName });
+  if (!token) {
+    res.status(403).json({
+      message: "token required for authentication",
+    });
+  } else {
+    try {
+      const authHeader = req.headers["authorization"];
+      const token = authHeader && authHeader.split(" ")[1];
+      const decodedToken = jwt.verify(token, secret);
+
+      req.decodedToken = decodedToken;
+    } catch (error) {
+      res.status(401).json({
+        message: "Invalid token received",
+      });
+    }
+  }
+  return next();
+};
 
 module.exports = verifyToken;
